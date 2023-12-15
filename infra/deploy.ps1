76a152815937a5afcd56a174e085a15e9a07cd59
Zoran Jovanovic <zojovano@microsoft.com>
2023-12-15 14:20:53 +0100

    added grpc call

--- infra/deploy.ps1
+++ infra/deploy.ps1
@@ -1,3 +1,5 @@
 az account set --subscription "90f5fdbd-fc04-461d-9c7b-df603d2efd6d"
 
 az containerapp env show --name "ContainerAppEnvironment02" --resource-group "platform-management" --query id --output tsv
+
+.\grpcurl.exe -plaintext vector.victoriousmushroom-c6e94d4a.westeurope.azurecontainerapps.io:6334 list
